67d10b7ba279f62d3e18a2fccdedfd6caa042b82
ALKelompok6 <128034627+ALKelompok6@users.noreply.github.com>
2023-03-17 15:45:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,18 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # Utils
 import joblib
 
+private_gsheets_url = "https://docs.google.com/spreadsheets/d/1lGqAx7sDL0B-nAl1Tlh3aPLWAYfcFdVSS_Y9_aU4VJc/edit?usp=sharing"
+
+[gcp_service_account]
+type = "service_account"
+project_id = "xxx"
+private_key_id = "xxx"
+private_key = "xxx"
+client_email = "xxx"
+client_id = "xxx"
+auth_uri = "https://accounts.google.com/o/oauth2/auth"
+token_uri = "https://oauth2.googleapis.com/token"
+auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
+client_x509_cert_url = "xxx"
 
 st.title("Data Pelatihan Jarak Jauh 2021 dan 2022")
