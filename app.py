65891189e0953e113d12266099b821979718dc55
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-24 16:07:00 +0700

    Coba lagi

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import joblib
 # Image
 from PIL import Image
 
-st.sidebar.title("Mana nih, katanya berubah")
+st.sidebar.title("Berarti cukup di klik push aja?")
 menu = ["EDA","Classification"]
 choice = st.sidebar.selectbox("Select Menu", menu)
 if choice == "EDA":
