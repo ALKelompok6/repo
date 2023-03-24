af3f1fe901723e748e1c43570390eef7fe202037
ALKelompok6 <128034627+ALKelompok6@users.noreply.github.com>
2023-03-24 14:39:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,24 +14,3 @@ import joblib
 
 from google.oauth2 import service_account
 from gsheetsdb import connect
-
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{https://docs.google.com/spreadsheets/d/1lGqAx7sDL0B-nAl1Tlh3aPLWAYfcFdVSS_Y9_aU4VJc/edit?usp=sharing}"')
-
