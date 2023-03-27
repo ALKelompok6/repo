1e184d469bf66d23f74ae1a8f48ff8601090a9ed
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-27 15:19:40 +0700

    cb

--- app.py
+++ app.py
@@ -28,15 +28,9 @@ st.title("Data Pelatihan 2021 dan 2022 Pusat Pendidikan dan Pelatihan Kepemimpin
 st.dataframe(df.head())
 
 st.subheader('INDEKS KESESUAIAN MATERI')
-st.line_chart(data=df,  x='NAMA PELATIHAN', y='KESESUAIAN MATERI', width=0, height=0, use_container_width=False)
+st.line_chart(data=df,  x='NAMA PELATIHAN', y=['KESESUAIAN MATERI', 'RENCANA PESERTA', 'TOTAL REALISASI PESERTA'], width=0, height=0, use_container_width=True)
 
 st.subheader('RENCANA PESERTA')
 hist_plot = df['RENCANA PESERTA'].plot.hist()
 st.write(hist_plot)
 st.pyplot()
-
-st.subheader('INDEKS KESESUAIAN MATERI 2')
-chart_data = pd.DataFrame(
-    'NAMA PELATIHAN',
-    columns=['KESESUAIAN MATERI', 'RENCANA PESERTA', 'TOTAL REALISASI PESERTA'])
-st.line_chart(chart_data)
