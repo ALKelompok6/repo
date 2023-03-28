eb7165ec8e09abb78df77dae6d0898c1d303347f
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-28 11:02:58 +0700

    cb

--- app.py
+++ app.py
@@ -62,21 +62,21 @@ realisasi_peserta_by_nama = (
 st.dataframe(realisasi_peserta_by_nama)
 st.bar_chart(data=realisasi_peserta_by_nama, x=['NAMA'], y=['RENCANA PESERTA', 'TOTAL REALISASI PESERTA'], width=0, height=0, use_container_width=True)
 
-filter_1 = (df['TAHUN']=='2021')
-#df.loc[~filter_1,:]
+filter_1 = (df['TAHUN']==2021)
+df.loc[~filter_1,:]
 
-st.subheader('TOTAL REALISASI PESERTA PER NAMA PELATIHAN 2021')
-realisasi_peserta_by_nama_2021 = (
-	df.loc[~filter_1,:].groupby(by=['NAMA']).sum()[['RENCANA PESERTA', 'TOTAL REALISASI PESERTA']].sort_values(by='NAMA')
-)
-st.dataframe(realisasi_peserta_by_nama_2021)
+#st.subheader('TOTAL REALISASI PESERTA PER NAMA PELATIHAN 2021')
+#realisasi_peserta_by_nama_2021 = (
+#	df.loc[~filter_1,:].groupby(by=['NAMA']).sum()[['RENCANA PESERTA', 'TOTAL REALISASI PESERTA']].sort_values(by='NAMA')
+#)
+#st.dataframe(realisasi_peserta_by_nama_2021)
 #st.bar_chart(data=realisasi_peserta_by_nama_2021, x=['NAMA'], y=['RENCANA PESERTA', 'TOTAL REALISASI PESERTA'], width=0, height=0, use_container_width=True)
 
 filter_2 = (df['TAHUN']==2022)
 df.loc[~filter_2,:]
 
-st.subheader('TOTAL REALISASI PESERTA PER NAMA PELATIHAN 2022')
-realisasi_peserta_by_nama_2022 = (
-	df.loc[~filter_2,:].groupby(by=['NAMA']).sum()[['RENCANA PESERTA', 'TOTAL REALISASI PESERTA']].sort_values(by='NAMA')
-)
-st.dataframe(realisasi_peserta_by_nama_2022)
+#st.subheader('TOTAL REALISASI PESERTA PER NAMA PELATIHAN 2022')
+#realisasi_peserta_by_nama_2022 = (
+#	df.loc[~filter_2,:].groupby(by=['NAMA']).sum()[['RENCANA PESERTA', 'TOTAL REALISASI PESERTA']].sort_values(by='NAMA')
+#)
+#st.dataframe(realisasi_peserta_by_nama_2022)
