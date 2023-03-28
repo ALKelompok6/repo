829e305b50b0f7b467df8ab942534153436ea85b
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-28 12:01:54 +0700

    cb

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if choice == "2021":
     st.dataframe(df.query(" `TAHUN` == 2.021 "))
 elif choice == "2022":
     st.subheader("Data Pelatihan 2022 Pusat Pendidikan dan Pelatihan Kepemimpinan dan Manajerial")
-    st.dataframe(df.query(" `TAHUN` == '2022' "))
+    st.dataframe(df.query(" `TAHUN` == 2.022 "))
 
 st.subheader('TOTAL RENCANA VS REALISASI PESERTA PER TAHUN')
 realisasi_peserta_by_tahun = (
