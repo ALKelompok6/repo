ccaf254dfaa36631b8a37aff7bab8cfea4837f65
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-28 08:55:07 +0700

    cb

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ df = load_data(st.secrets["public_gsheets_url"])
 st.title("Data Pelatihan 2021 dan 2022 Pusat Pendidikan dan Pelatihan Kepemimpinan dan Manajerial")
 st.dataframe(df, width=1360)
 st.subheader('REALISASI PESERTA')
-st.line_chart(data=df, x=df['TAHUN'], y=df['TOTAL REALISASI PESERTA'], width=1360, height=0, use_container_width=True)
+st.line_chart(data=df, x=data['TAHUN'], y=data['TOTAL REALISASI PESERTA'], width=1360, height=0, use_container_width=True)
 
 #sns.barplot(x=data['Survived'].value_counts().index, y=data['Survived'].value_counts())
 
