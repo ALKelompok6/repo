73126b77bb62041226031c8a7adbd122af490d0d
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-28 14:24:05 +0700

    cd

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ df = load_data(st.secrets["public_gsheets_url"])
 
 total_rencana = int(df['RENCANA PESERTA'].sum())
 total_peserta = int(df['TOTAL REALISASI PESERTA'].sum())
-rerata_seseusaian_materi =':star:' * round(df['KESESUAIAN MATERI'].mean(), 2)
+rerata_seseusaian_materi = round(df['KESESUAIAN MATERI'].mean(), 2)
+indeks_bintang =':star:' * int(round(rerata_seseusaian_materi, 0))
 #total_jamlator
 
 left_column, middle_column, right_column = st.columns(3)
@@ -52,7 +53,7 @@ with middle_column:
     st.subheader(f"{total_peserta:,}")
 with right_column:
     st.subheader('KESESUAIAN MATERI')
-    st.subheader(f"{rerata_seseusaian_materi:,}")
+    st.subheader(f"{rerata_seseusaian_materi:,} {indeks_bintang}")
 
 st.sidebar.header('Tahun')
 menu = ["2021","2022"]
