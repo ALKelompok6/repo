6dfb755a5286fb34760ebdd6ae2bd2714ed90992
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-03-28 13:19:24 +0700

    cd

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ if choice == "2021":
 
     st.subheader('PESERTA PER STATUS')
     peserta_per_unit = (
-	df.query(" `TAHUN` == '2,021' ").groupby(by=['NAMA']).sum()[['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI']].sort_values(by='NAMA')
+	df.query(" `TAHUN` == '2,021' ").groupby(by=['NAMA']).sum()[['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI', 'PERSENTASE KEIKUTSERTAAN']].sort_values(by='NAMA')
     )
     st.dataframe(peserta_per_unit)
-    st.line_chart(data=peserta_per_unit, x=['NAMA'], y=['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI', 'TELAH MENGIKUTI/LULUS'/'TOTAL REALISASI PESERTA'], width=0, height=0, use_container_width=True)    
+    st.line_chart(data=peserta_per_unit, x=['NAMA'], y=['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI', 'PERSENTASE KEIKUTSERTAAN'], width=0, height=0, use_container_width=True)    
 elif choice == "2022":
     st.subheader("Data Pelatihan 2022 Pusat Pendidikan dan Pelatihan Kepemimpinan dan Manajerial")
     st.dataframe(df.query(" `TAHUN` == '2,022' "))
@@ -79,10 +79,10 @@ elif choice == "2022":
 
     st.subheader('PESERTA PER STATUS')
     peserta_per_unit = (
-	df.query(" `TAHUN` == '2,022' ").groupby(by=['NAMA']).sum()[['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI']].sort_values(by='NAMA')
+	df.query(" `TAHUN` == '2,022' ").groupby(by=['NAMA']).sum()[['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI', 'PERSENTASE KEIKUTSERTAAN']].sort_values(by='NAMA')
     )
     st.dataframe(peserta_per_unit)
-    st.line_chart(data=peserta_per_unit, x=['NAMA'], y=['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI', 'TELAH MENGIKUTI/LULUS'/'TOTAL REALISASI PESERTA'], width=0, height=0, use_container_width=True) 
+    st.line_chart(data=peserta_per_unit, x=['NAMA'], y=['TELAH MENGIKUTI/LULUS', 'TIDAK MEMENUHI SYARAT', 'MENGUNDURKAN DIRI',	'TIDAK MENGIKUTI', 'PERSENTASE KEIKUTSERTAAN'], width=0, height=0, use_container_width=True) 
 
 st.subheader('TOTAL RENCANA VS REALISASI PESERTA PER TAHUN')
 realisasi_peserta_by_tahun = (
