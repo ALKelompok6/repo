b48fc4f2aada3659f20abdfec2e136256c118904
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-04-10 13:51:24 +0700

    cb

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ tahun = st.sidebar.multiselect(
 bulan = st.sidebar.selectbox(
     "Pilih Bulan:",
     options=df["NAMA BULAN"].unique(),
-    default=["Januari"]
+    default='January'
 )
 
 nama = st.sidebar.multiselect(
