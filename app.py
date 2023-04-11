645538e707758beebd0ce43636f146d71968000e
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-04-11 09:46:23 +0700

    cb

--- app.py
+++ app.py
@@ -98,10 +98,10 @@ st.dataframe(df_selection[['NAMA PELATIHAN', 'TAHUN', 'KESESUAIAN MATERI',	'HARI
 #RENCANA VS REALISASI PESERTA PER PELATIHAN
 st.subheader('RENCANA VS REALISASI PESERTA PER PELATIHAN')
 realisasi_peserta_by_nama = (
-df_selection.groupby(by=['BULAN']).sum()[['RENCANA PESERTA', 'TOTAL REALISASI PESERTA']].sort_values(by='BULAN')
+df_selection.groupby(by=['BULAN']).sum()[['RENCANA PESERTA', 'TOTAL REALISASI PESERTA', 'PERSENTASE KEIKUTSERTAAN']].sort_values(by='BULAN')
 )
 st.dataframe(realisasi_peserta_by_nama)
-st.line_chart(data=realisasi_peserta_by_nama, x=['BULAN'], y=['RENCANA PESERTA', 'TOTAL REALISASI PESERTA'], width=0, height=0, use_container_width=True)
+st.line_chart(data=realisasi_peserta_by_nama, x=['BULAN'], y=['RENCANA PESERTA', 'TOTAL REALISASI PESERTA', 'PERSENTASE KEIKUTSERTAAN'], width=0, height=0, use_container_width=True)
 
 st.subheader('RENCANA VS REALISASI JAMLATOR PER PELATIHAN')
 realisasi_jamlator_by_nama = (
