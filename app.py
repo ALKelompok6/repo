0b7f45f2d941e2b0c1bd8e1f1ee1460ec628146b
ALKelompok6 <prepostevalap.ppsdm@gmail.com>
2023-04-11 09:51:57 +0700

    cb

--- app.py
+++ app.py
@@ -112,10 +112,10 @@ st.line_chart(data=realisasi_jamlator_by_nama, x=['BULAN'], y=['RENCANA JAMLATOR
 
 st.subheader('PERSENTASE KEIKUTSERTAAN DAN REALISASI JAMLATOR')
 persentase_keikutsertaan_by_bulan = (
-df_selection.groupby(by=['BULAN']).sum()[['PERSENTASE KEIKUTSERTAAN', 'PERSENTASE JAMLATOR']].sort_values(by='BULAN')
+df_selection.groupby(by=['NAMA']).sum()[['PERSENTASE KEIKUTSERTAAN', 'PERSENTASE JAMLATOR']].sort_values(by='NAMA')
 )
 st.dataframe(persentase_keikutsertaan_by_bulan)
-st.bar_chart(data=persentase_keikutsertaan_by_bulan, x=['BULAN'], y=['PERSENTASE KEIKUTSERTAAN', 'PERSENTASE JAMLATOR'], width=0, height=0, use_container_width=True)
+st.bar_chart(data=persentase_keikutsertaan_by_bulan, x=['NAMA'], y=['PERSENTASE KEIKUTSERTAAN', 'PERSENTASE JAMLATOR'], width=0, height=0, use_container_width=True)
 
 
 st.subheader('INDEKS KESESUAIAN MATERI')
